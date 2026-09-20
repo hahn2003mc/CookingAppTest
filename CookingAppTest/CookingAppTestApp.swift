26c521e21ee35922d095eeb193177a83e18bff58
Molly Huang <myh5513@psu.edu>
2026-09-20 15:00:54 -0400

    Initial Commit

---
+++ CookingAppTest/CookingAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppTestApp.swift
+//  CookingAppTest
+//
+//  Created by Molly Huang on 9/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CookingAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
